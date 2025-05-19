d1f1099044078be1ee49644b66880fed7df251e6
Thomas Stroman <tomstroman@users.noreply.github.com>
2025-05-18 23:35:32 -0600

    Add test coverage for main

---
+++ tests/test_main.py
@@ -0,0 +1,16 @@
+from fastapi.testclient import TestClient
+
+from app.main import fastapi_app as app
+
+
+def test_app_instance__no_code():
+    client = TestClient(app)
+    response = client.get("/game_state/foo/")
+    assert response.status_code == 404
+
+
+def test_logging_configured():
+    import logging
+
+    logger = logging.getLogger()
+    assert any(isinstance(h, logging.StreamHandler) for h in logger.handlers)
